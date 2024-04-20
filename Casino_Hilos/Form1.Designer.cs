a9ab5519f53c6aa8fc41d8af61682f60a9cf021f
StarSer <androidemoc@gmail.com>
2024-04-20 08:48:45 -0700

    Base del proyecto

--- Casino_Hilos/Form1.Designer.cs
+++ Casino_Hilos/Form1.Designer.cs
@@ -28,13 +28,74 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(256, 115);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(120, 395);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(382, 115);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(120, 395);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(508, 115);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(134, 395);
+            this.panel3.TabIndex = 2;
+            // 
+            // panel4
+            // 
+            this.panel4.Location = new System.Drawing.Point(648, 115);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(128, 395);
+            this.panel4.TabIndex = 3;
+            // 
+            // panel5
+            // 
+            this.panel5.Location = new System.Drawing.Point(782, 115);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(127, 395);
+            this.panel5.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1085, 694);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Slot Machine";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- Casino_Hilos/Form1.cs
+++ Casino_Hilos/Form1.cs
@@ -12,9 +12,135 @@ namespace Casino_Hilos
 {
     public partial class Form1 : Form
     {
+        private Panel[] paneles;
+        private PictureBox[][,] cuadrosImagen;
+        private Random aleatorio;
+        private int duracionAnimacion = 10000; // Duración de la animación en milisegundos (10 segundos)
+        private int tiempoTranscurrido = 0; // Tiempo transcurrido desde el inicio de la animación
+        private int anchoPictureBox = 100;
+        private int altoPictureBox = 100;
+        private int espaciado = 20; // Espacio entre PictureBox
+        private int numFilas = 3; // Número de filas
+        private int numColumnas = 1; // Número de columnas
+        private int numPaneles = 5; // Número de paneles
+
         public Form1()
         {
             InitializeComponent();
+            InicializarPaneles();
+            InicializarCuadrosImagen();
+            InicializarTemporizador();
+        }
+
+        private void InicializarPaneles()
+        {
+            paneles = new Panel[numPaneles];
+            paneles[0] = panel1;
+            paneles[1] = panel2;
+            paneles[2] = panel3;
+            paneles[3] = panel4;
+            paneles[4] = panel5;
+        }
+
+        private void InicializarCuadrosImagen()
+        {
+            cuadrosImagen = new PictureBox[numPaneles][,];
+
+            for (int k = 0; k < numPaneles; k++)
+            {
+                cuadrosImagen[k] = new PictureBox[numColumnas, numFilas]; // 1 columna y 3 filas
+
+                for (int i = 0; i < numColumnas; i++)
+                {
+                    for (int j = 0; j < numFilas; j++)
+                    {
+                        cuadrosImagen[k][i, j] = new PictureBox();
+                        cuadrosImagen[k][i, j].Size = new Size(anchoPictureBox, altoPictureBox);
+                        cuadrosImagen[k][i, j].Location = new Point(0, 0 + j * (altoPictureBox + espaciado));
+                        cuadrosImagen[k][i, j].SizeMode = PictureBoxSizeMode.StretchImage;
+                        paneles[k].Controls.Add(cuadrosImagen[k][i, j]);
+                    }
+                }
+            }
+        }
+
+        private void InicializarTemporizador()
+        {
+            timer1 = new Timer();
+            timer1.Interval = 50; // Animación más rápida (50 milisegundos)
+            timer1.Tick += Timer1_Tick;
+            aleatorio = new Random();
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            tiempoTranscurrido += timer1.Interval; // Actualizar el tiempo transcurrido
+
+            if (tiempoTranscurrido >= duracionAnimacion)
+            {
+                timer1.Stop(); // Detener la animación después de 10 segundos
+                return;
+            }
+
+            // Desplaza las imágenes hacia abajo en cada panel
+            for (int k = 0; k < numPaneles; k++)
+            {
+                for (int i = 0; i < numColumnas; i++)
+                {
+                    for (int j = 0; j < numFilas; j++)
+                    {
+                        cuadrosImagen[k][i, j].Top += 20; // Mover las imágenes más rápido
+
+                        // Si la imagen ha alcanzado la parte inferior, vuelve a la parte superior y cambia la imagen
+                        if (cuadrosImagen[k][i, j].Top >= paneles[k].Height)
+                        {
+                            cuadrosImagen[k][i, j].Top = cuadrosImagen[k][i, j].Top - (altoPictureBox + espaciado) * numFilas;
+                            cuadrosImagen[k][i, j].Image = ObtenerImagenAleatoria();
+                        }
+                    }
+                }
+            }
+        }
+
+        private Image ObtenerImagenAleatoria()
+        {
+            int numeroAleatorio = aleatorio.Next(1, 4);
+            Image imagen = null;
+
+            switch (numeroAleatorio)
+            {
+                case 1:
+                    imagen = Properties.Resources.Limones;
+                    break;
+                case 2:
+                    imagen = Properties.Resources.Naranja;
+                    break;
+                case 3:
+                    imagen = Properties.Resources.Sandia;
+                    break;
+                default:
+                    break;
+            }
+
+            return imagen;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Inicializa las imágenes con una imagen aleatoria en cada panel
+            for (int k = 0; k < numPaneles; k++)
+            {
+                for (int i = 0; i < numColumnas; i++)
+                {
+                    for (int j = 0; j < numFilas; j++)
+                    {
+                        cuadrosImagen[k][i, j].Image = ObtenerImagenAleatoria();
+                    }
+                }
+            }
+
+            timer1.Start();
         }
     }
+
 }
